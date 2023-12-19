d12175a2947baaef66aee8560a6e0775d278283c
Artemiy <aptem64@yandex.ru>
2023-12-19 21:34:06 +0300

    completed first hw and 2th hw

---
+++ HomeWork_01/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace HomeWork_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if(args.Length == 0)
+            {
+                Console.WriteLine("Введены нулевые аргументы");
+            }
+            else if(args.Length == 3) 
+            {
+                if(float.TryParse(args[0], out float firstNumber) && float.TryParse(args[2], out float secondNumber))
+                {
+                    if (char.TryParse(args[1], out char mathSym))
+                    {
+                        switch (mathSym)
+                        {
+                            case '+':
+                                Console.WriteLine($"{firstNumber + secondNumber:f2}");
+                                break;
+                            case '-':
+                                Console.WriteLine($"{firstNumber - secondNumber:f2}");
+                                break;
+                            case '/':
+                                Console.WriteLine($"{firstNumber / secondNumber:f2}");
+                                break;
+                            case '*':
+                                Console.WriteLine($"{firstNumber * secondNumber:f2}");
+                                break;
+                            default:
+                                Console.WriteLine("Некорректный арифметический символ\nДопустимые символы: + - * /");
+                                break;
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Арифметический знак не может быть длинее одного символа");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Один или оба аргументы не являются числом");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Неверное количество аргументов");
+                Console.WriteLine("Введите 3 аргумента, где\n1-й аргумент - первое число\n2-й аргумент - арифметический знак\n3-й аргумент - второе число");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ HomeWork_02/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace HomeWork_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] a = { { 7, 3, 2 }, { 4, 9, 6 }, { 1, 8, 5 } };
+            PrintArr(a);
+            SortTwiceArr(a);
+            Console.WriteLine();
+            PrintArr(a);
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Сортировка двумерного массива
+        /// </summary>
+        /// <param name="nums">двумерный массив чисел</param>
+        private static void SortTwiceArr(int[,] nums)
+        {
+            var res = new List<int>();
+            int count = 0;
+
+            foreach (var x in nums)
+            {
+                res.Add(x);
+            }
+            res.Sort();
+
+            for (int i = 0; i < nums.GetLength(0); i++)
+            {
+                for (int j = 0; j < nums.GetLength(1); j++)
+                {
+                    nums[i, j] = res[count++];
+                }
+            }
+
+        }
+
+        /// <summary>
+        /// Вывод массива в консоль
+        /// </summary>
+        /// <param name="nums">входящий массив</param>
+        private static void PrintArr(int[,] nums)
+        {
+            for (int i = 0; i < nums.GetLength(0); i++)
+            {
+                Console.Write("| ");
+                for (int j = 0; j < nums.GetLength(1); j++)
+                {
+                    Console.Write(nums[i, j] + " | ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
